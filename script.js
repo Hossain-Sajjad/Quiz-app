bdfc4e5f0797fe769ae4203476429c4cf2459863
Hossain-Sajjad <sunshinems58.com>
2023-04-03 05:38:43 +0600

    worked on multiple choice feature

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const questionSet = [
         option2: 'Uniform Resource Location',
         option3: 'Universal Response Locator',
         option4: 'Universal Response Location',
-        answer: 1
+        answer: [1, 3, 2]
     },
     {
         id: 2,
@@ -16,7 +16,7 @@ const questionSet = [
         option2: 'Facebook',
         option3: 'Firefox',
         option4: 'Chrome',
-        answer: 2
+        answer: [2]
     }
 ]
 
@@ -27,7 +27,25 @@ function addQuestion() {
     const option2 = document.getElementById('option2').value;
     const option3 = document.getElementById('option3').value;
     const option4 = document.getElementById('option4').value;
-    const answer = document.getElementById('answer').value;
+
+    const answer1 = document.getElementById("answer1");
+    const answer2 = document.getElementById("answer2");
+    const answer3 = document.getElementById("answer3");
+    const answer4 = document.getElementById("answer4");
+
+    let answer = [];
+    if (answer1.checked == true) {
+        answer.push(parseInt(document.getElementById("answer1").value))
+    }
+    if (answer2.checked == true) {
+        answer.push(parseInt(document.getElementById("answer2").value))
+    }
+    if (answer3.checked == true) {
+        answer.push(parseInt(document.getElementById("answer3").value))
+    }
+    if (answer4.checked == true) {
+        answer.push(parseInt(document.getElementById("answer4").value))
+    }
 
     const id = questionSet.length + 1;
 
@@ -39,9 +57,9 @@ function addQuestion() {
         option2: option2,
         option3: option3,
         option4: option4,
-        answer: parseInt(answer),
+        answer,
     };
-
+    console.log(obj)
     questionSet.push(obj);
 
     // showing all question from array after adding a new question 
@@ -52,7 +70,6 @@ function addQuestion() {
     document.getElementById('option2').value = '';
     document.getElementById('option3').value = '';
     document.getElementById('option4').value = '';
-    document.getElementById('answer').value = '';
 }
 
 // showing the initial questions
@@ -69,12 +86,13 @@ function showQuestion() {
         <div class="container">
             ${question.question}
             <hr>
-            <input type="radio" ${'name = ' + '"question' + question.id + '"'} ${question.answer == 1 ? 'id = ' + '"correct' + question.id + '"' : ''}>${question.option1}<br>
-            <input type="radio" ${'name = ' + '"question' + question.id + '"'} ${question.answer == 2 ? 'id = ' + '"correct' + question.id + '"' : ''}>${question.option2}<br>
-            <input type="radio" ${'name = ' + '"question' + question.id + '"'} ${question.answer == 3 ? 'id = ' + '"correct' + question.id + '"' : ''}>${question.option3}<br>
-            <input type="radio" ${'name = ' + '"question' + question.id + '"'} ${question.answer == 4 ? 'id = ' + '"correct' + question.id + '"' : ''}>${question.option4}<br>
+            <div><input type="checkbox" ${question.answer.indexOf(1) != -1 ? 'class = ' + '"correct' + question.id + '"' : 'class = ' + '"wrong' + question.id + '"'}>${question.option1}</div><br>
+            <div><input type="checkbox" ${question.answer.indexOf(2) != -1 ? 'class = ' + '"correct' + question.id + '"' : 'class = ' + '"wrong' + question.id + '"'}>${question.option2}</div><br>
+            <div><input type="checkbox" ${question.answer.indexOf(3) != -1 ? 'class = ' + '"correct' + question.id + '"' : 'class = ' + '"wrong' + question.id + '"'}>${question.option3}</div><br>
+            <div><input type="checkbox" ${question.answer.indexOf(4) != -1 ? 'class = ' + '"correct' + question.id + '"' : 'class = ' + '"wrong' + question.id + '"'}>${question.option4}</div><br>
         </div>
         `
+        console.log(div.innerHTML)
         quizList.appendChild(div);
     }
 }
@@ -83,11 +101,32 @@ function showQuestion() {
 function result() {
     let score = 0;
     for (question of questionSet) {
+        let flag = 1;
+
         const answerId = 'correct' + question.id;
-        if (document.getElementById(answerId).checked) {
+        const rightElements = document.getElementsByClassName(answerId);
+        for (element of rightElements) {
+            if (element.checked == false) {
+                flag = 0;
+            }
+        }
+
+        const wrongId = 'wrong' + question.id;
+        const wrongElements = document.getElementsByClassName(wrongId);
+        for (element of wrongElements) {
+            if (element.checked == true) {
+                flag = 0;
+                console.log(element)
+                element.parentElement.setAttribute("class", "demo-class");
+            }
+        }
+
+        if (flag == 1) {
             score++;
         }
+
     }
+
     document.getElementById('result').innerHTML = '';
     const div = document.createElement('div');
     div.innerHTML = `<h3>Score : ${score}`
